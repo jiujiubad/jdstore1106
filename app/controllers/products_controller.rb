98af88f92b1a14980e1176e3f249b9cedc5a2703
谢育初 <524945638@qq.com>
2017-11-05 21:26:34 +0800

    add_to_cart button

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,9 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def add_to_cart
+    @product = Product.find(params[:id])
+    redirect_to :back, notice: "测试成功"
+  end
 end
